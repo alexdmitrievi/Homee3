30e65d336b7fa98085d3d68d42c510323187fcba
alexdmitrievi <103923985+alexdmitrievi@users.noreply.github.com>
2022-05-31 12:35:42 +0600

    and

---
+++ Home7/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int[] array = new int[8];
+Random rnd = new Random();
+for (int i = 0; i < 8; i++){
+    array[i] = rnd.Next(0,2);
+    Console.WriteLine($"{array[i]}");
+}
